d9534545086f81079fc5dfb5f406e694e6e31b89
mohamedyousufs <yousufmohamed1999@gmail.com>
2026-01-09 09:44:59 +0530

    Revert "masjid commit"
    This reverts commit f2ebfef275a14148f8e381fc354145c74ed496c7.
    
    Masjid commit revert because of prpject unstructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
-import { useState } from "react";
-import "./index.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login";
 
-const App = () => {
-  const [isPassword, setIsPassword] = useState(true);
-
+function App() {
   return (
-    <div className="container">
-      <div className="left-panel">
-        <h1>Masjid Hub</h1>
-        <p>Connecting Our Community</p>
-      </div>
+    <Router>
+      <Routes>
+        {/* Default route "/" goes to Login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
-      <div className="right-panel">
-        <h2>Log In</h2>
+        {/* Login route */}
+        <Route path="/login" element={<Login />} />
 
-        <form className="login-form">
-          <div className="input-group">
-            <span className="material-symbols-outlined">mail</span>
-            <input type="text" placeholder="UserName" required />
-          </div>
-
-          <div className="input-group password-group">
-            <span className="material-symbols-outlined">lock</span>
-            <input
-              type={isPassword ? "password" : "text"}
-              placeholder="Password"
-              required
-            />
-            <button
-              type="button"
-              className="password-toggle material-symbols-outlined"
-              onClick={() => setIsPassword(!isPassword)}
-              aria-label={isPassword ? "Show password" : "Hide password"}
-            >
-              {isPassword ? "visibility" : "visibility_off"}
-            </button>
-          </div>
-
-          <button className="login-btn" type="submit">
-            Continue
-          </button>
-        </form>
-      </div>
-    </div>
+        {/* Catch-all route redirects to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
